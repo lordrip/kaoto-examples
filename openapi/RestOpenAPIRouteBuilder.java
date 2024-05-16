f4994d44039a279368ce37613e9068aa68957fa7
Ricardo M <lordrip@gmail.com>
2024-05-16 14:17:41 +0000

    chore: Remove example routes

--- openapi/RestOpenAPIRouteBuilder.java
+++
@@ -1,8 +0,0 @@
-import org.apache.camel.builder.RouteBuilder;
-
-public class RestOpenAPIRouteBuilder extends RouteBuilder {
-
-    public void configure() throws Exception {
-        restConfiguration().apiContextPath("/q/openapi");
-    }
-}
